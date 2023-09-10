acc9c26a03f9b32ac323a768fc1adf060c060b41
Kowalska Marta <kamarta0991@gmail.com>
2023-09-10 20:08:07 +0200

    add project init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,155 @@
-import Notiflix from 'notiflix';
+import { fetchImages, pageLimit } from './js/pixabay-api';
 
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { Loading } from 'notiflix/build/notiflix-loading-aio';
 import 'notiflix/dist/notiflix-3.2.6.min.css';
+
+const searchForm = document.querySelector('#search-form');
+const formInput = document.querySelector('#search-form [name="searchQuery"]');
+const gallery = document.querySelector('.gallery');
+const loadMore = document.querySelector('#load-more');
+const scrollTop = document.querySelector('#scroll-top');
+
+let gallerySimpleLightBox = new SimpleLightbox('.photo-card a', {
+  captions: true,
+  captionsData: 'alt',
+  captionsDelay: 500,
+});
+
+let page = 1;
+let numberOfPage = 1;
+let inputValue = null;
+
+function generateHTMLphotoCard(searchResults) {
+  const myPictureHTML = searchResults.map(
+    ({
+      webformatURL,
+      largeImageURL,
+      tags,
+      likes,
+      views,
+      comments,
+      downloads,
+    }) => {
+      return `<div class="photo-card">
+      <a href="${largeImageURL}">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+        </a>
+        <div class="info">
+          <p class="info-item">
+            <b>Likes</b>
+            ${likes}
+          </p>
+          <p class="info-item">
+            <b>Views</b>
+            ${views}
+          </p>
+          <p class="info-item">
+            <b>Comments</b>
+            ${comments}
+          </p>
+          <p class="info-item">
+            <b>Downloads</b>
+            ${downloads}
+          </p>
+        </div>
+      </div>`;
+    }
+  );
+
+  gallery.insertAdjacentHTML('beforeend', myPictureHTML.join(''));
+}
+
+async function getImages(page, inputValue) {
+  try {
+    const images = await fetchImages(inputValue, page);
+    const searchResult = images.hits;
+    console.log(searchResult);
+
+    numberOfPage = Math.ceil(images.totalHits / pageLimit);
+    console.log(numberOfPage);
+
+    if (images.totalHits === 0) {
+      Notify.failure(
+        `Sorry, there are no images matching your search query. Please try again.`
+      );
+      return;
+    }
+
+    if (images.totalHits !== 0) {
+      Notify.success(`Hooray! We found ${images.totalHits} images.`);
+      generateHTMLphotoCard(searchResult);
+      gallerySimpleLightBox.refresh();
+    }
+
+    if (images.totalHits > pageLimit) {
+      loadMore.classList.remove('is-hidden');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function deleteGallery(gallery) {
+  while (gallery.firstChild) {
+    gallery.firstChild.remove();
+  }
+}
+
+async function onSubmitForm(event) {
+  event.preventDefault();
+  deleteGallery(gallery);
+  page = 1;
+  inputValue = formInput.value;
+  loadMore.classList.add('is-hidden');
+
+  console.log(inputValue);
+
+  if (inputValue === '') {
+    Notify.failure('Write something to search...');
+    return;
+  }
+
+  event.currentTarget.reset();
+
+  try {
+    await getImages(page, inputValue);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function displayMorePhoto() {
+  if (page === numberOfPage) {
+    Notify.info("We're sorry, but you've reached the end of search results.");
+    loadMore.classList.add('is-hidden');
+    return;
+  } else {
+    Loading.pulse('Loading data, please wait...');
+    page += 1;
+    try {
+      const images = await fetchImages(inputValue, page);
+      const searchResult = images.hits;
+      console.log(searchResult);
+      generateHTMLphotoCard(searchResult);
+      gallerySimpleLightBox.refresh();
+    } catch (error) {
+      console.log(error);
+    }
+    Loading.remove();
+  }
+}
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+}
+
+searchForm.addEventListener('submit', onSubmitForm);
+loadMore.addEventListener('click', displayMorePhoto);
+scrollTop.addEventListener('click', scrollToTop);
--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,8 +1,30 @@
 import axios from 'axios';
 
-const AUTH_TOKEN =
-  '39289578-81c44a31b0c9c16ae76a18111';
-axios.defaults.headers.common['x-api-key'] = AUTH_TOKEN;
+const AUTH_TOKEN = '39289578-81c44a31b0c9c16ae76a18111';
+// axios.defaults.headers.common['x-api-key'] = AUTH_TOKEN;  //not working with pixabay, only key
 
-const API_CAT_URL = 'https://api.thecatapi.com/v1';
+const API_CAT_URL = 'https://pixabay.com/api/';
 
+const pageLimit = 40;
+
+const fetchImages = async (queryToFetch, pagetoFetch) => {
+  try {
+    const { data } = await axios.get(API_CAT_URL, {
+      params: {
+        key: AUTH_TOKEN,
+        q: queryToFetch,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: pagetoFetch,
+        per_page: pageLimit,
+      },
+    });
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { fetchImages, pageLimit };
